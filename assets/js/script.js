bdce23f072776c6effea9badfa643afe082d4cd1
DasunMadawa <dasunmadawa8864@gmail.com>
2024-05-03 16:01:40 +0530

    add sale and item styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,23 @@
 $(".toggle-btn-2").on("click", () => {
     $(".side-bar-body").hide();
-    $(".customer-details-wrapper").show();
+    $(".side-bar-body-2").show();
 
-    toggleBtnColorChanger($(".toggle-btn-2") , $(".toggle-btn-1"));
+    toggleBtnColorChanger($(".toggle-btn-2"), $(".toggle-btn-1"));
 
 });
 
 $(".toggle-btn-1").on("click", () => {
-    $("#side-bar-body").show();
-    $("#customer-details-wrapper").hide();
+    $(".side-bar-body").show();
+    $(".side-bar-body-2").hide();
 
-    toggleBtnColorChanger($(".toggle-btn-1") , $(".toggle-btn-2"));
+    toggleBtnColorChanger($(".toggle-btn-1"), $(".toggle-btn-2"));
 
 });
 
 $(".toggle-btn-1").click();
 
 // $("#customer-toggle-btn").click();
-function toggleBtnColorChanger(btn1 , btn2) {
+function toggleBtnColorChanger(btn1, btn2) {
     btn1.css({"backgroundColor": "var(--light-green)"});
     btn2.css({"backgroundColor": "unset"});
 
@@ -29,5 +29,77 @@ function toggleBtnColorChanger(btn1 , btn2) {
 // $("#filter-btn").hide();
 // $("#body-item-wrapper").hide();
 
-$("#sale-sec").hide();
+function navigateSections(section) {
+    $("section").hide();
+
+    section.show();
+
+}
+
+// navigateSections($(".sale-sec"));
+
+// $("#admin-panel-nav-btn").click(function () {
+//     navigateSections($(""));
+//
+// });
+
+$("#sales-nav-btn").click(function () {
+    console.log("helloooooo");
+    navigateSections($("#sale-sec"));
+
+});
+
+$("#items-nav-btn").click(function () {
+    navigateSections($("#item-sec"));
+
+});
+
+$("#customers-nav-btn").click(function () {
+    navigateSections($("#customer-sec"));
+
+});
+//
+// $("#suppliers-nav-btn").click(function () {
+//     navigateSections($(""));
+//
+// });
+//
+// $("#employees-nav-btn").click(function () {
+//     navigateSections($(""));
+//
+// });
+//
+// $("#resupply-nav-btn").click(function () {
+//     navigateSections($(""));
+//
+// });
+//
+// $("#refund-nav-btn").click(function () {
+//     navigateSections($(""));
+//
+// });
+
+$("#sale-history-btn").click(function () {
+   $("#order-history-body-wrapper").toggle();
+   $("#body-item-wrapper").toggle();
+
+});
+
+// $("#item-customer-toggle-set > div").click(function () {
+//    $(".item-details-wrapper").hide();
+//    // $("").toggle();
+//
+// });
+
+$("#save-item-btn").click(function () {
+    $("#item-add-wrapper").toggle();
+
+});
+
+$("#add-new-item-btn").click(function () {
+    $("#item-add-wrapper").toggle();
+});
+
+$("#customers-nav-btn").click();
+
 
