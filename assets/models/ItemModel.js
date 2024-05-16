fa3854f4549f9ab1db4e40e02d6ee973448cb004
DasunMadawa <dasunmadawa8864@gmail.com>
2024-05-17 01:45:46 +0530

    bug fix

---
+++ assets/models/ItemModel.js
@@ -0,0 +1,31 @@
+export class ItemModel {
+    constructor(
+
+    ) {
+        this.supplierId = supplierId;
+        this.supplierName = supplierName;
+        this.supplierCategory = supplierCategory;
+        this.mobileNo = mobileNo;
+        this.landLineNo = landLineNo;
+        this.email = email;
+        this.addressNoOrName = addressNoOrName;
+        this.addressLane = addressLane;
+        this.addressCity = addressCity;
+        this.addressState = addressState;
+        this.postalCode = postalCode;
+        this.country = country;
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
